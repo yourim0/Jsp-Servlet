8b69276c6d2a7caa64f89b733130c08060b5abc1
yourim0 <kyourim0@gmail.com>
2022-12-12 18:16:27 +0900

    SMTP

--- MailSendProject/src/smtp/NaverSMTP.java
+++ MailSendProject/src/smtp/NaverSMTP.java
@@ -34,7 +34,7 @@ public class NaverSMTP {
 			
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("kyo9721", "Ehrtnfl46!");
+				return new PasswordAuthentication("네이버 아이디", "네이버 비밀번호");
 			}
 		};
 	}
